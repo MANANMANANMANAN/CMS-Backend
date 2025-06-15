d26fd6b5221b6b428bb99617c8dbb7f19819374e
amankum2004 <kumaraman6012@gmail.com>
2025-06-15 13:32:25 +0530

    backend

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -18,19 +18,59 @@ const prisma = new PrismaClient();
 //   }
 // };
 
-exports.getRegistrations = async (req, res) => {
+exports.getRegistrations =  async (req, res) => {
   try {
-    const result = await prisma.student_registered.findMany({
-      where:{
-        status: "I"
+    const { branch, program, rollno, status } = req.query;
+
+    const filters = {};
+
+    if (branch) filters.branch = branch;
+    if (program) filters.program = program;
+    if (rollno) filters.student_id = rollno;
+
+    const data = await prisma.student_pre_registered.findMany({
+      where: {
+        accept_reject: true,
+        student: {
+          ...filters,
+        },
       },
-      include: { student: true, course: true }
+      include: {
+        student: true,
+        course: true,
+      },
     });
+
+    const result = data.map((entry) => ({
+      student_id: entry.student.student_id,
+      student_name: entry.student.student_name,
+      branch: entry.student.branch,
+      program: entry.student.program,
+      course_code: entry.course.course_code,
+      course_name: entry.course.course_name,
+      status: entry.accept_reject ? "Accepted" : "Pending",
+    }));
+
     res.json(result);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  } catch (err) {
+    console.error("Error fetching preregistration data", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+// exports.RegisteredStudentsDetails = async (req, res) => {
+//   try {
+//     const result = await prisma.student_registered.findMany({
+//       where:{
+//         status: "I"  // students with grade "I"
+//       },
+//       include: { student: true, course: true }
+//     });
+//     res.json(result);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// };
 
 // Only show accepted students (registered)
 // router.get('/faculty/courses/:courseId/registrations', async (req, res) => {
