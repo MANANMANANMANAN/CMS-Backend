04bdd6f37bd9736d917452c49e783359a08c97e9
amankum2004 <kumaraman6012@gmail.com>
2025-06-15 13:14:08 +0530

    backend

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -18,41 +18,44 @@ const prisma = new PrismaClient();
 //   }
 // };
 
-// exports.getRegistrations = async (req, res) => {
-//   try {
-//     const result = await prisma.student_registered.findMany({
-//       include: { student: true, course: true }
-//     });
-//     res.json(result);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
+exports.getRegistrations = async (req, res) => {
+  try {
+    const result = await prisma.student_registered.findMany({
+      where:{
+        status: "I"
+      },
+      include: { student: true, course: true }
+    });
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 // Only show accepted students (registered)
 // router.get('/faculty/courses/:courseId/registrations', async (req, res) => {
-exports.getRegistrations =  async (req, res) => {
-  const { courseId } = req.params;
+// exports.getRegistrations =  async (req, res) => {
+//   const { courseId } = req.params;
 
-  try {
-    const registered = await prisma.student_pre_registered.findMany({
-      where: {
-        course_id: courseId,
-        accept_reject: true,
-      },
-      include: { student: true },
-    });
+//   try {
+//     const registered = await prisma.student_pre_registered.findMany({
+//       where: {
+//         course_id: courseId,
+//         accept_reject: true,
+//       },
+//       include: { student: true },
+//     });
 
-    const formatted = registered.map((entry) => ({
-      student_id: entry.student_id,
-      name: entry.student.student_name,
-      branch: entry.student.branch,
-      program: entry.student.program,
-      status: 'Accepted',
-    }));
+//     const formatted = registered.map((entry) => ({
+//       student_id: entry.student_id,
+//       name: entry.student.student_name,
+//       branch: entry.student.branch,
+//       program: entry.student.program,
+//       status: 'Accepted',
+//     }));
 
-    res.json(formatted);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch registered students.' });
-  }
-};
+//     res.json(formatted);
+//   } catch (err) {
+//     res.status(500).json({ error: 'Failed to fetch registered students.' });
+//   }
+// };
